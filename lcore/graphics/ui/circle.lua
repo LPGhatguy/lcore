004f7b11e3c4a58817550bdf0b75efaa46a732d1
Lucien Greathouse <me@lpghatguy.com>
2014-02-19 12:24:44 -0700

    Add ui.circle and circle_shape

---
+++ lcore/graphics/ui/circle.lua
@@ -0,0 +1,35 @@
+--[[
+#id graphics.ui.circle
+#title UI Circle
+#status production
+#version 1.0
+
+#desc A circle with a border
+]]
+
+local L = (...)
+local oop = L:get("utility.oop")
+local color = L:get("graphics.color")
+local circle_shape = L:get("graphics.ui.circle_shape")
+local circle
+
+circle = oop:class(circle_shape)({
+	background_color = color:get("darkgray"),
+	border_color = color:get("silver"),
+	border_width = 2,
+
+	draw = function(self)
+		circle_shape.draw(self)
+
+		local bw = self.border_width
+		local hw = bw / 2
+		love.graphics.setLineWidth(bw)
+		love.graphics.setColor(self.border_color)
+		love.graphics.circle("line", self.x - hw, self.y - hy, self.r + bw)
+
+		love.graphics.setColor(self.background_color)
+		love.graphics.circle("fill", self.x, self.y, self.r)
+	end
+})
+
+return circle
