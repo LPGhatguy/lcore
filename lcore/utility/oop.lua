de107a6491a3f05383504472c3cb4e830188fa42
Lucien Greathouse <me@lpghatguy.com>
2014-02-19 12:25:35 -0700

    Add oop::mix and remove static objects

--- lcore/utility/oop.lua
+++ lcore/utility/oop.lua
@@ -1,16 +1,23 @@
 --[[
 #id utility.oop
 #title OOP Interface
-#version 1.2
+#version 1.3
 #status production
 
 #desc Provides object orientation to the framework.
+
+##todo Optimize oop.mix
 ]]
 
 local L = (...)
 
-local oop
 local etable = L:get("utility.table")
+local oop
+
+--Checks an argument for whether it's a type or a type identifier
+local resolve = function(item)
+	return (type(item) == "table") and item or L:get(tostring(item))
+end
 
 oop = {
 	--@method objectify
@@ -33,26 +40,69 @@ oop = {
 		end
 	end,
 
-	--@method static
-	static = function(self, ...)
-		local new = etable.merge(self.static_object, {})
-		new:inherit(...)
+	--@method mix
+	mix = function(self, ...)
+		local result = {}
+		local mixing = {}
+		local args = {...}
 
-		return function(target)
-			etable:merge(new, target)
-			return target
+		for key, value in pairs(args) do
+			args[key] = resolve(value)
 		end
+
+		for step, object in pairs(args) do
+			for key, value in pairs(object) do
+				local typed = type(value)
+
+				if (typed == "function") then
+					if (not mixing[key]) then
+						mixing[key] = {value}
+					elseif (not etable:contains(mixing[key], value)) then
+						table.insert(mixing[key], value)
+					end
+				elseif (not result[key]) then
+					if (typed == "table") then
+						result[key] = etable:deepcopy(value)
+					else
+						result[key] = value
+					end
+				end
+			end
+		end
+
+		for key, value in pairs(mixing) do
+			if (#value > 1) then
+				result[key] = function(...)
+					local result
+					for index, func in pairs(value) do
+						result = {func(...)}
+					end
+
+					return unpack(result)
+				end
+			else
+				result[key] = value[1]
+			end
+		end
+
+		return result
 	end,
 
-	--@class object
+	--/@class object
+	--documentation doesn't work with non-method member items
 	object = {
 		inherit = function(self, ...)
 			local args = {...}
+
 			for key = 1, #args do
-				etable:merge(args[key], self)
+				etable:merge(resolve(args[key]), self)
 			end
 
 			return self
+		end,
+
+		copy = function(self)
+			return etable:deepcopy(self)
 		end,
 
 		new = function(self, ...)
@@ -63,28 +113,12 @@ oop = {
 			end
 		end,
 
-		copy = function(self)
-			return etable:deepcopy(self)
-		end,
-
 		destroy = function(self, ...)
 			if (self._destroy) then
 				return self:_destroy(...)
 			else
 				return true
 			end
-		end
-	},
-
-	--@class static_object
-	static_object = {
-		inherit = function(self, ...)
-			local args = {...}
-			for key = 1, #args do
-				etable:merge(args[key], self)
-			end
-
-			return self
 		end
 	}
 }
