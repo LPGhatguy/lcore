44a54ce0466ec3f5bc9d89aa53ffe6aa20d27fd7
Lucien Greathouse <me@lpghatguy.com>
2014-03-02 21:40:17 -0700

    ui.rectangle objects' borders can be toggled

--- lcore/graphics/ui/rectangle.lua
+++ lcore/graphics/ui/rectangle.lua
@@ -15,17 +15,20 @@ local rectangle
 
 rectangle = oop:class(rectangle_shape)({
 	background_color = color:get("darkgray"),
+	border = true,
 	border_color = color:get("silver"),
 	border_width = 2,
 
 	draw = function(self)
 		rectangle_shape.draw(self)
 
-		local bw = self.border_width
-		local hw = bw / 2
-		love.graphics.setLineWidth(bw)
-		love.graphics.setColor(self.border_color)
-		love.graphics.rectangle("line", self.x - hw, self.y - hw, self.w + bw, self.h + bw)
+		if (self.border) then
+			local bw = self.border_width
+			local hw = bw / 2
+			love.graphics.setLineWidth(bw)
+			love.graphics.setColor(self.border_color)
+			love.graphics.rectangle("line", self.x - hw, self.y - hw, self.w + bw, self.h + bw)
+		end
 
 		love.graphics.setColor(self.background_color)
 		love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
