18b044af15ac2f4fe40f7fa16bfa5ed20ed256d3
Lucien Greathouse <me@lpghatguy.com>
2014-03-08 23:14:39 -0700

    Update utility.filesystem to be more responsive

--- lcore/utility/filesystem.lua
+++ lcore/utility/filesystem.lua
@@ -5,49 +5,101 @@
 #staus incomplete
 
 #desc Wraps I/O procedures to bring APIs together
+
+#note The vanilla/LFS implementation of 'list' is less efficient than the love one.
+#note 'iterate' has more predictable performance.
 ]]
 
 local L = (...)
-local fs
+local lfs_enabled, lfs = pcall(require, "lfs")
+local fs = {}
+local fs_interface = {}
 
-if (L.platform == "love") then
-	fs = {
-		read = love.filesystem.read,
-		write = love.filesystem.write
-	}
-else
-	fs = {
-		read = function(path)
-			local handle = io.open(path, "r")
+local value_iterator = function(source)
+	local key
 
-			if (handle) then
-				local body = handle:read()
-				handle:close()
-
-				return body
-			end
-		end,
-
-		write = function(path, body)
-			local handle = io.open(path, "w")
-
-			if (handle) then
-				handle:write(body)
-				handle:close()
-
-				return true
-			end
-		end,
-
-		exists = function(path)
-			local handle = io.open(path, "r")
-
-			if (handle) then
-				handle:close()
-				return true
-			end
-		end
-	}
+	return function()
+		key, value = next(source, key)
+		return value
+	end
 end
+
+fs["love"] = {
+	read = love.filesystem.read,
+	write = love.filesystem.write,
+	exists = love.filesystem.exists,
+	list = love.filesystem.getDirectoryItems,
+	iterate = function(path)
+		return value_iterator(love.filesystem.getDirectoryItems(path))
+	end
+}
+
+fs["lua"] = {
+	read = function(path)
+		local handle, errorstring = io.open(path, "r")
+
+		if (handle) then
+			local body = handle:read()
+			handle:close()
+
+			return body
+		else
+			return nil, errorstring
+		end
+	end,
+
+	write = function(path, body)
+		local handle, errorstring = io.open(path, "w")
+
+		if (handle) then
+			handle:write(body)
+			handle:close()
+
+			return true
+		else
+			return nil, errorstring
+		end
+	end,
+
+	exists = function(path)
+		local handle, errorstring = io.open(path, "r")
+
+		if (handle) then
+			handle:close()
+			return true
+		else
+			return nil, errorstring
+		end
+	end,
+
+	list = function(path)
+		if (lfs_enabled) then
+			local paths = {}
+
+			for path in lfs.dir(path) do
+				table.insert(paths, path)
+			end
+
+			return paths
+		else
+			--well, oops
+			return nil, "Unable to list without love or LFS available!"
+		end
+	end,
+
+	iterate = function(path)
+		return lfs.dir(path)
+	end
+}
+
+setmetatable(fs_interface, {
+	__index = function(self, key)
+		if (fs[L.platform]) then
+			return fs[L.platform][key]
+		else
+			return fs["lua"][key]
+		end
+	end
+})
 
 return fs
