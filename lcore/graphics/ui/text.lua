a8ca85eeeede4c954f6c3b24ab6c6172d8aad1e6
Lucien Greathouse <me@lpghatguy.com>
2014-05-01 13:35:42 -0600

    Replace textlabel with ui.text and ui.text_shape

---
+++ lcore/graphics/ui/text.lua
@@ -0,0 +1,43 @@
+local L, this = ...
+this.title = "UI Text Label"
+this.version = "2.0"
+this.status = "production"
+this.desc = "Draws text."
+
+local oop = L:get("lcore.utility.oop")
+local color = L:get("lcore.graphics.color")
+local font = L:get("lcore.graphics.font")
+local text_shape = L:get("lcore.graphics.ui.text_shape")
+local label
+
+label = oop:class(text_shape)({
+	text_color = color:get("white"),
+
+	draw = function(self)
+		text_shape.draw(self)
+
+		local w = self.font:getWidth(self.text)
+		local h = self.font:getHeight()
+
+		love.graphics.setFont(self.font)
+		love.graphics.setColor(self.text_color)
+
+		local y = self.y
+
+		if (self.valign == "bottom") then
+			y = self.y - h
+		elseif (self.valign == "center") then
+			y = self.y - h / 2
+		end
+
+		if (self.align == "right") then
+			love.graphics.printf(self.text, self.x - w, y, w, "right")
+		elseif (self.align == "center") then
+			love.graphics.printf(self.text, self.x - w / 2, y, w, "center")
+		else
+			love.graphics.print(self.text, self.x, y)
+		end
+	end
+})
+
+return label
---
+++ lcore/graphics/ui/text_shape.lua
@@ -0,0 +1,41 @@
+local L, this = ...
+this.title = "UI Text Shape"
+this.version = "1.0"
+this.status = "production"
+this.desc = "Provides functionality for handling text."
+
+local oop = L:get("lcore.utility.oop")
+local element = L:get("lcore.graphics.ui.element")
+local font = L:get("lcore.graphics.font")
+local label
+
+label = oop:class(element)({
+	text = "",
+	align = "center", -- {left, center, right}
+	valign = "center", -- {top, center, bottom}
+	font = nil,
+
+	_new = function(base, self, manager, text, x, y, h, align)
+		self = element._new(base, self, manager, x, y)
+
+		self.font = font:get(h)
+		self.text = text or self.text
+		self.align = align or self.align
+
+		return self
+	end,
+
+	_resize = function(self, h)
+		self.font = font:get(h)
+	end,
+
+	contains = function(self, x, y)
+		local w = self.font:getWidth(self.text)
+		local h = self.font:getHeight()
+
+		return (x >= self.x and x <= self.x + w and
+			y >= self.y and y <= self.y + h)
+	end
+})
+
+return label
