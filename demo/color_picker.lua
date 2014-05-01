6ae327acd8ae2848301c1a04609277eba70dbe93
Lucien Greathouse <me@lpghatguy.com>
2014-05-01 13:37:50 -0600

    Add a color picker demo

---
+++ demo/color_picker.lua
@@ -0,0 +1,14 @@
+--[[
+Usage: require("demo.color_picker")
+
+Shows off use of the built-in color picker object
+]]
+
+local L = require("lcore.core")
+local event = L:get("lcore.service.event")
+local color_picker = L:get("lcore.graphics.ui.complex.color_picker")
+local platform = L:get("lcore.platform.interface"):get()
+
+color_picker:new(event.global, 50, 50, 600, 480)
+
+platform:init()
