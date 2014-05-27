ca9476679a50293f57ccb76b36801a9b27b98b80
Lucien Greathouse <me@lpghatguy.com>
2014-05-26 18:27:02 -0600

    Internal graphics.ui.image changes

--- lcore/graphics/ui/image.lua
+++ lcore/graphics/ui/image.lua
@@ -6,10 +6,9 @@ this.desc = "Draws an image with an optional background and border."
 
 local oop = L:get("lcore.utility.oop")
 local rectangle = L:get("lcore.graphics.ui.rectangle")
-local color = L:get("lcore.graphics.color")
 local image
 
-image = oop:class(rectangle)({
+image = oop:class(rectangle) {
 	image = nil,
 
 	_new = function(base, self, manager, image, x, y, w, h)
@@ -22,16 +21,13 @@ image = oop:class(rectangle)({
 	draw = function(self)
 		rectangle.draw(self)
 
-		love.graphics.setColor(255, 255, 255)
-
 		if (self.image) then
-			if (self.w and self.h) then
-				love.graphics.draw(self.image, self.x, self.y, 0, self.w / self.image:getWidth(), self.h / self.image:getHeight())
-			else
-				love.graphics.draw(self.image, self.x, self.y)
-			end
+			local iw, ih = self.image:getDimensions()
+
+			love.graphics.setColor(255, 255, 255)
+			love.graphics.draw(self.image, self.x, self.y, 0, self.w / iw, self.h / ih)
 		end
 	end
-})
+}
 
 return image
